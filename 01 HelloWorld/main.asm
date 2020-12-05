691959f83615b37ec60632fc09fe3ec7cac73d87
padnest <padnest@gmail.com>
2020-12-05 08:38:13 +0100

    first commit

---
+++ 01 HelloWorld/main.asm
@@ -0,0 +1,24 @@
+
+BasicUpstart2(start)
+
+start:
+	lda #0
+    sta $D020
+    sta $D021
+	// kernal clear screen routine
+	jsr $e544
+
+	ldx #0	
+!:	
+	lda HelloMsg, x
+	beq exit
+	sta $0400, x
+	inx
+	bne !-
+exit:
+	rts
+
+HelloMsg:
+.encoding "screencode_upper"
+.text "HELLO WORLD!"
+.byte 0
